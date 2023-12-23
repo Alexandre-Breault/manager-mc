470c39014590024782851e3165556970a8d439d2
Alexandre BREAULT <creepminecraft53@gmail.com>
2023-12-23 10:26:23 +0100

    connection db

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,6 @@
+import { sql } from "@vercel/postgres";
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request: Request) {}
 export async function POST(request: Request) {
-    
+    const {row} = await sql `INSERT INTO `
 }
